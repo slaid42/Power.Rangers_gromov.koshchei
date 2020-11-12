3087621240487cb7f0b9bed15314236af61d4d61
Art-1313 <gromov.ak@phystech.edu>
2020-11-12 23:09:27 +0300

    create classes and describe them

---
+++ solar_system.py
@@ -0,0 +1,67 @@
+import pygame
+
+pygame.init()
+
+
+class Sun():
+    """
+    Set Sun characteristics, draw it
+    """
+    pass
+
+
+class Planet():
+    """
+    Rule Planets movements, draw them, set their characteristics
+    """
+    def __init__(self, number):
+        """
+        Initialize planet speed, coordinate etc.
+        :param number: choose planet from list in file
+        """
+        pass
+    pass
+
+
+class OuterSpaceBody():
+    """
+    Create random body from outer space
+    Rule its movements and draw it
+    """
+    pass
+
+
+class StartButton():
+   """
+   Draw start button, check when you press it and start simulation
+   """
+   pass
+
+
+class PauseButton():
+   """
+   Draw pause button, check when you press it and pause simulation
+   """
+   pass
+
+
+class ResetButton():
+   """
+   Draw reset button, check when you press it
+   and put planets in initial positions
+   """
+   pass
+
+
+class ScaleSlider():
+    """
+    Draw slider to change scale
+    """
+    pass
+
+
+class Manager():
+    """
+    Handle all events
+    """
+    pass
